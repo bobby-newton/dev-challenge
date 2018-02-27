1a01b9dbe44eccc72f97076bd00fae8a8e32630a
Chris Cooper <cjcooper252@gmail.com>
2018-02-27 14:39:27 +0000

    sets up express server

---
+++ back-end/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes/routes.js");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+routes(app);
+
+const port = 3000;
+
+const server = app.listen(port, function () {
+    console.log("app running on port", server.address().port);
+});
